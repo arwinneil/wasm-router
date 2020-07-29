32b6fca9218f0a3941b1afebbe020c13ce2d4ac0
Arwin Neil Baichoo <arwinneil@gmail.com>
2020-07-29 13:14:56 +0400

    Full Routing Implementation

--- src/router.rs
+++ src/router.rs
@@ -22,15 +22,14 @@ impl Router {
     pub fn init(&self) {
         let routes: Vec<(String, fn(s: &str))> = self.routes.clone();
 
+        //Hash routing forward in history and URL rewrite
         let handle_hash = Closure::wrap(Box::new(move |_evt: web_sys::Event| {
-            // let current_path = Self::get_fragment();
-
             let l = web_sys::window().unwrap().location();
             let l: String = l.hash().unwrap().chars().skip(1).collect();
             log(&["hash handle : ", l.as_str()].concat());
 
             let h = web_sys::window().unwrap().history().unwrap();
-            h.push_state_with_url(&JsValue::NULL, "", Some(l.as_str()));
+            h.replace_state_with_url(&JsValue::NULL, "", Some(l.as_str()));
 
             if routes.iter().any(|r| r.0 == l) {
                 let index = routes.iter().position(|r| r.0 == l).unwrap();
@@ -45,6 +44,41 @@ impl Router {
             .set_onhashchange(Some(handle_hash.as_ref().unchecked_ref()));
 
         handle_hash.forget();
+
+        let routes: Vec<(String, fn(s: &str))> = self.routes.clone();
+
+        //Routing for navigating in history and escaping hash routes
+        let handle_pop = Closure::wrap(Box::new(move |_evt: web_sys::Event| {
+            let l = Self::get_fragment();
+
+            if web_sys::window()
+                .unwrap()
+                .location()
+                .hash()
+                .unwrap()
+                .chars()
+                .count()
+                > 0
+            {
+                log("hash detected");
+                return ();
+            }
+
+            log(&["pop handle : ", l.as_str()].concat());
+
+            if routes.iter().any(|r| r.0 == l) {
+                let index = routes.iter().position(|r| r.0 == l).unwrap();
+                routes[index].1(routes[index].0.as_str());
+            } else {
+                log("hit nothing");
+            }
+        }) as Box<dyn Fn(_)>);
+
+        web_sys::window()
+            .unwrap()
+            .set_onpopstate(Some(handle_pop.as_ref().unchecked_ref()));
+
+        handle_pop.forget();
     }
 
     pub fn add(&mut self, route: &str, handler: fn(s: &str)) {
